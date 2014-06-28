727160607fd58c741ba760a02548b6a447f5ce2f
James Meisler <james@mindfulware.com>
2014-06-28 09:22:15 -0500

    Run redmine plugin creation.  Update init.rb

---
+++ init.rb
@@ -0,0 +1,8 @@
+Redmine::Plugin.register :redmine_activity_tools do
+  name 'Redmine Activity Tools plugin'
+  author 'James Meisler'
+  description 'A plugin for extending the Redmine API to report activity.'
+  version '0.0.1'
+  url 'https://github.com/jamesmeisler/redmine_activity_tools'
+  author_url 'htt://hobbitpi.com'
+end
